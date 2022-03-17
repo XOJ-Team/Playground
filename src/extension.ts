b7a0b26b80533fefce6cc4d9dc3f6cfa7242d80e
isnowify <isnowify@outlook.com>
2022-03-17 23:11:03 +0800

    feat: Activate plugin at first run

--- src/extension.ts
+++ src/extension.ts
@@ -1,38 +1,21 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-let myStatusBarItem: vscode.StatusBarItem;
+let connectionStatsBarItem: vscode.StatusBarItem;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('"xoj-playground" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let testCommand = vscode.commands.registerCommand('xoj-playground.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from XOJ Playground!');
-	});
 
 	// register a command that is invoked when extension is activated
 	const connectionStatsCommandName = 'xoj-playground.showConnectionStatus';
 	let connectionStatsCommand = vscode.commands.registerCommand(connectionStatsCommandName, () => {
+		// TODO:check connection status to XOJ backend 
 		vscode.window.showInformationMessage(`Connected to XOJ Server.`);
 	});
 
-	// create a new status bar item that we can now manage
-	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-	myStatusBarItem.command = connectionStatsCommandName;
-	context.subscriptions.push(myStatusBarItem);
+	connectionStatsBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	connectionStatsBarItem.command = connectionStatsCommandName;
+	context.subscriptions.push(connectionStatsBarItem);
 	context.subscriptions.push(connectionStatsCommand);
-	context.subscriptions.push(testCommand);
 
 	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
 	context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem));
@@ -47,6 +30,6 @@ export function deactivate() {
 }
 
 function updateStatusBarItem(): void {
-	myStatusBarItem.text = `$(megaphone) Connected to XOJ Server`;
-	myStatusBarItem.show();
+	connectionStatsBarItem.text = `$(megaphone) Connected to XOJ Server`;
+	connectionStatsBarItem.show();
 }
