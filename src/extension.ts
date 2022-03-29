b873a282fb5b0815cfa823ca42efcd1a2b39bf7f
unknown <Sakai Kazuki>
2022-03-29 20:34:28 +0800

    refactor: ActionCenter is now a separate module

--- src/extension.ts
+++ src/extension.ts
@@ -6,21 +6,7 @@ import { ActionCenterView } from './ui/actionCenter';
 export function activate(context: vscode.ExtensionContext) {
 
 	const statusIndicator = new StatusIndicator(context);
-
-	const actionCenter = new ActionCenterView(context.extensionUri);
-
-	context.subscriptions.push(
-		vscode.window.registerWebviewViewProvider(ActionCenterView.viewType, actionCenter));
-
-	context.subscriptions.push(
-		vscode.commands.registerCommand('calicoColors.addColor', () => {
-			actionCenter.addColor();
-		}));
-
-	context.subscriptions.push(
-		vscode.commands.registerCommand('calicoColors.clearColors', () => {
-			actionCenter.clearColors();
-		}));
+	const actionCenter = new ActionCenterView(context);
 
 	console.log('[INFO] XOJ Playground is now active!');
 
--- src/ui/actionCenter.ts
+++ src/ui/actionCenter.ts
@@ -2,67 +2,81 @@ import * as vscode from 'vscode';
 
 export class ActionCenterView implements vscode.WebviewViewProvider {
 
-	public static readonly viewType = 'playground.actionCenter';
+    public static readonly viewType = 'playground.actionCenter';
 
-	private _view?: vscode.WebviewView;
+    private _view?: vscode.WebviewView;
 
-	constructor(
-		private readonly _extensionUri: vscode.Uri,
-	) { }
+    constructor(
+        private readonly _extensionContext: vscode.ExtensionContext
+    ) {
+        _extensionContext.subscriptions.push(
+            vscode.window.registerWebviewViewProvider(ActionCenterView.viewType, this)
+        );
+        _extensionContext.subscriptions.push(
+            vscode.commands.registerCommand('calicoColors.addColor', () => {
+                this.addColor();
+            })
+        );
+        _extensionContext.subscriptions.push(
+            vscode.commands.registerCommand('calicoColors.clearColors', () => {
+                this.clearColors();
+            })
+        );
+    }
 
-	public resolveWebviewView(
-		webviewView: vscode.WebviewView,
-		context: vscode.WebviewViewResolveContext,
-		_token: vscode.CancellationToken,
-	) {
-		this._view = webviewView;
+    public resolveWebviewView(
+        webviewView: vscode.WebviewView,
+        context: vscode.WebviewViewResolveContext,
+        _token: vscode.CancellationToken,
+    ) {
+        this._view = webviewView;
 
-		webviewView.webview.options = {
-			// Allow scripts in the webview
-			enableScripts: true,
+        webviewView.webview.options = {
+            // Allow scripts in the webview
+            enableScripts: true,
 
-			localResourceRoots: [
-				this._extensionUri
-			]
-		};
+            localResourceRoots: [
+                this._extensionContext.extensionUri
+            ]
+        };
 
-		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+        webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
 
-		webviewView.webview.onDidReceiveMessage(data => {
-			switch (data.type) {
-				case 'colorSelected':
-					{
-						vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(`#${data.value}`));
-						break;
-					}
-			}
-		});
-	}
+        webviewView.webview.onDidReceiveMessage(data => {
+            switch (data.type) {
+                case 'colorSelected':
+                    {
+                        vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(`#${data.value}`));
+                        break;
+                    }
+            }
+        });
+    }
 
-	public addColor() {
-		if (this._view) {
-			this._view.show?.(true); // `show` is not implemented in 1.49 but is for 1.50 insiders
-			this._view.webview.postMessage({ type: 'addColor' });
-		}
-	}
+    public addColor() {
+        if (this._view) {
+            this._view.show?.(true); // `show` is not implemented in 1.49 but is for 1.50 insiders
+            this._view.webview.postMessage({ type: 'addColor' });
+        }
+    }
 
-	public clearColors() {
-		if (this._view) {
-			this._view.webview.postMessage({ type: 'clearColors' });
-		}
-	}
+    public clearColors() {
+        if (this._view) {
+            this._view.webview.postMessage({ type: 'clearColors' });
+        }
+    }
 
-	private _getHtmlForWebview(webview: vscode.Webview) {
-		// Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
-		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'assets', 'main.js'));
+    private _getHtmlForWebview(webview: vscode.Webview) {
+        // Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
+        const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionContext.extensionUri, 'assets', 'main.js'));
 
-		// Do the same for the stylesheet.
-		const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'assets', 'reset.css'));
-		const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'assets', 'vscode.css'));
-		const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'assets', 'main.css'));
+        // Do the same for the stylesheet.
+        const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionContext.extensionUri, 'assets', 'reset.css'));
+        const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionContext.extensionUri, 'assets', 'vscode.css'));
+        const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionContext.extensionUri, 'assets', 'main.css'));
 
-		// Use a nonce to only allow a specific script to be run.
-		const nonce = getNonce();
+        // Use a nonce to only allow a specific script to be run.
+        const nonce = getNonce();
 
         return `<!DOCTYPE html>
 			<html lang="en">
@@ -101,14 +115,14 @@ export class ActionCenterView implements vscode.WebviewViewProvider {
 				<script nonce="${nonce}" src="${scriptUri}"></script>
 			</body>
 			</html>`;
-	}
+    }
 }
 
 function getNonce() {
-	let text = '';
-	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	for (let i = 0; i < 32; i++) {
-		text += possible.charAt(Math.floor(Math.random() * possible.length));
-	}
-	return text;
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < 32; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
 }
