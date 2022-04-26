85da2a32c94a1e2dc966c7a2753879e6449791fd
isnowify <isnowify@outlook.com>
2022-04-26 18:12:45 +0800

    feat(resultView): init resultView in panel

--- src/ui/ResultView.ts
+++ src/ui/ResultView.ts
@@ -4,40 +4,86 @@ import { SubmissionObject } from '../api/Types';
 
 export class ResultView {
 
-    private static readonly _viewType = 'xoj-playground.resultView';
+    private static readonly _viewType = 'playground.panel.resultView';
 
     constructor(private readonly _extensionContext: vscode.ExtensionContext) {
-
-
-        // const resultDocumentProvider = new ResultDocumentProvider(vscode.window.activeTextEditor);\
-
-        // vscode.debug.registerDebugConfigurationProvider('xoj', new DebugConfiguration());
-        // vscode.debug.onDidStartDebugSession(() => {
-        // 	console.log('[DEBUG] Debug session started');
-        // });
+		const view = vscode.window.createTreeView(ResultView._viewType, { treeDataProvider: aNodeWithIdTreeDataProvider(), showCollapseAll: true });
+		_extensionContext.subscriptions.push(view);
     }
 
-    // public async resolveCustomTextEditor(
-    //     document: vscode.TextDocument,
-    //     webviewPanel: vscode.WebviewPanel,
-    //     _token: vscode.CancellationToken
-    // ): Promise<void> {
+}
 
-    // }
 
-    public async show() {
+const tree = {
+	'a': {
+		'aa': {
+			'aaa': {
+				'aaaa': {
+					'aaaaa': {
+						'aaaaaa': {
 
-        const fileUri = vscode.Uri.file("/home/nuc/test.md");
-        await vscode.commands.executeCommand("markdown.showLockedPreviewToSide", fileUri);
+						}
+					}
+				}
+			}
+		},
+		'ab': {}
+	},
+	'b': {
+		'ba': {},
+		'bb': {}
+	}
+};
 
-        // vscode.window.showTextDocument(editor.document, {
-        //     viewColumn: vscode.ViewColumn.Beside
-        // });
-    }
 
-    public log() {
-        console.log(vscode.workspace.textDocuments);
-        console.log(vscode.window.visibleTextEditors);
-    }
+function aNodeWithIdTreeDataProvider(): vscode.TreeDataProvider<{ key: string }> {
+	return {
+		getChildren: (element: { key: string }): { key: string }[] => {
+			return getChildren(element ? element.key : "Not ").map(key => getNode(key));
+		},
+		getTreeItem: (element: { key: string }): vscode.TreeItem => {
+			const treeItem = getTreeItem(element.key);
+			treeItem.id = element.key;
+			return treeItem;
+		},
+		getParent: ({ key }: { key: string }): { key: string } => {
+			const parentKey = key.substring(0, key.length - 1);
+			return  new Key(parentKey);
+		}
+	};
+}
 
+function getChildren(key: string): string[] {
+	if (!key) {
+		return Object.keys(tree);
+	}
+	const treeElement = getTreeElement(key);
+	if (treeElement) {
+		return Object.keys(treeElement);
+	}
+	return [];
+}
+
+function getTreeItem(key: string): vscode.TreeItem {
+	const treeElement = getTreeElement(key);
+	// An example of how to use codicons in a MarkdownString in a tree item tooltip.
+	const tooltip = new vscode.MarkdownString(`$(zap) Tooltip for ${key}`, true);
+	return {
+		label: /**vscode.TreeItemLabel**/<any>{ label: key, highlights: key.length > 1 ? [[key.length - 2, key.length - 1]] : void 0 },
+		tooltip,
+		collapsibleState: treeElement && Object.keys(treeElement).length ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None
+	};
+}
+
+function getTreeElement(element: string): any {
+	let parent = tree;
+	return "Tree element";
+}
+
+function getNode(key: string): { key: string } {
+	return new Key(key);
+}
+
+class Key {
+	constructor(readonly key: string) { }
 }
