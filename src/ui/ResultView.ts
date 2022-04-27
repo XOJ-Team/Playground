e7e5ca42ca6cb402d3a113e94ddbd005f0bcbf41
isnowify <isnowify@outlook.com>
2022-04-27 16:47:35 +0800

    unfix: event trigger

--- src/ui/ResultView.ts
+++ src/ui/ResultView.ts
@@ -9,18 +9,34 @@ import { JudgeServer } from '../api/Judge';
 export class ResultView {
 	private _disposable: vscode.Disposable;
 	private static readonly _showResultCommand = 'xoj-playground.showResult';
-	private static readonly _refreshResultCommand = 'xoj-playground.showResult';
+	private static readonly _refreshResultCommand = 'xoj-playground.refreshResult';
 	private static readonly _viewType = 'playground.panel.resultView';
-	//TODO: Replace with event/command
-	private _lookupResponse: Judge0LookupResponse = { "status_id": 3, "time": "0.002", "memory": 784, "compile_output": null, "status": { "id": 3, "description": "Accepted" } } as Judge0LookupResponse;
+	private _submissionResultModelList: SubmissionResultModel[] = [];
+
+	private _lookupResponse: Judge0LookupResponse = { "status_id": 88, "time": "0.002", "memory": 784, "compile_output": null, "status": { "id": 3, "description": "Accepted" } } as Judge0LookupResponse;
 	private onResultFetched(_lookupResponse: Judge0LookupResponse) {
 		this._lookupResponse = _lookupResponse;
+		this._initSubmissionResultModelList;
 		console.log('[INFO] onResultFetched');
 		console.log(this._lookupResponse);
-		vscode.commands.executeCommand(ResultView._refreshResultCommand);
+		vscode.commands.executeCommand(ResultView._refreshResultCommand,this._submissionResultModelList);
 	}
+
+	private _initSubmissionResultModelList(lookupResponse: Judge0LookupResponse) {
+		let k: keyof Judge0LookupResponse;
+		for (k in lookupResponse) {
+			if (k === 'status') {
+				this._submissionResultModelList.push(new SubmissionResultModel('Status', lookupResponse.status.description, 'check'));
+			}
+			else {
+				this._submissionResultModelList.push(new SubmissionResultModel(k, lookupResponse[k]));
+			}
+		}
+	}
+
 	constructor(private readonly _extensionContext: vscode.ExtensionContext) {
-		const resultDataProvider = new ResultDataProvider(this._lookupResponse);
+		this._initSubmissionResultModelList(this._lookupResponse);
+		const resultDataProvider = new ResultDataProvider(this._submissionResultModelList);
 		this._disposable = vscode.commands.registerCommand(ResultView._showResultCommand, this.onResultFetched, this);
 		_extensionContext.subscriptions.push(this._disposable);
 		this._disposable = vscode.commands.registerCommand(ResultView._refreshResultCommand, resultDataProvider.refresh, this);
@@ -44,28 +60,18 @@ export class SubmissionResultModel {
 
 
 export class ResultDataProvider implements vscode.TreeDataProvider<SubmissionResultModel> {
-	private _onDidChangeTreeData: vscode.EventEmitter<any> = new vscode.EventEmitter<any>();
-	readonly onDidChangeTreeData: vscode.Event<any> = this._onDidChangeTreeData.event;
+	private _onDidChangeTreeData: vscode.EventEmitter<SubmissionResultModel[] | undefined | null | void> = new vscode.EventEmitter<SubmissionResultModel[] | undefined | null | void>();
+	readonly onDidChangeTreeData: vscode.Event<SubmissionResultModel[] | undefined | null | void> = this._onDidChangeTreeData.event;
 	private _submissionResultModelList: SubmissionResultModel[] = [];
-	private _initSubmissionResultModelList(lookupResponse: Judge0LookupResponse) {
-		let k: keyof Judge0LookupResponse;
-		for (k in lookupResponse) {
-			if (k === 'status') {
-				this._submissionResultModelList.push(new SubmissionResultModel('Status', lookupResponse.status.description, 'check'));
-			}
-			else {
-				this._submissionResultModelList.push(new SubmissionResultModel(k, lookupResponse[k]));
-			}
-		}
+
+
+	constructor(private readonly submissionResultModelList: SubmissionResultModel[]) {
+		this._submissionResultModelList = submissionResultModelList;
 	}
 
-
-	constructor(private readonly _lookupResponse: Judge0LookupResponse) {
-		this._initSubmissionResultModelList(_lookupResponse);
-	}
-
-	public refresh(): any {
-		this._onDidChangeTreeData.fire(void 0);
+	public refresh(submissionResultModelList: SubmissionResultModel[]): any {
+		this._submissionResultModelList = submissionResultModelList;
+		this._onDidChangeTreeData.fire(undefined);
 	}
 
 	public getTreeItem(element: SubmissionResultModel): vscode.TreeItem {
